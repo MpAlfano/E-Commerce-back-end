ab48e6afe48697a0d3b53984644c09ba64671dbd
Mark Alfano <mpalfano4@gmail.com>
2022-02-26 16:21:59 -0500

    finished product routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const TagAdd = await Tag.create(req.body);
+    res.status(200).json(TagAdd);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
